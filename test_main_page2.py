9a83fdaa756494d21ee57023872860db7b204860
Khabibullev <khabibullaev023@gmail.com>
2024-09-11 16:59:07 +0500

    new changes

--- test_main_page2.py
+++ test_main_page2.py
@@ -4,4 +4,5 @@ def test_guest_can_go_to_login_page(driver):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(driver, link)
     page.open()
-    page.go_to_login_page()
+    #page.go_to_login_page()
+    page.go_to_login_link()
